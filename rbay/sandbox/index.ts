452d6badca2391207736e4c66c9ca535f762c31c
unsivil <unsivilaudio@gmail.com>
2022-06-18 09:55:22 -0600

    end of section 5

--- rbay/sandbox/index.ts
+++ rbay/sandbox/index.ts
@@ -1,5 +1,22 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+	await client.hSet('car', {
+		color: 'red',
+		year: 1950,
+		engine: JSON.stringify({ cylinders: 8 }),
+		owner: 'null',
+		service: 'undefined'
+	});
+
+	const car = await client.hGetAll('car');
+
+	if (Object.keys(car).length === 0) {
+		return;
+	}
+
+	console.log(car);
+};
+
 run();
