266bfac023325380c5c9299b6e90b8510bcedd6b
unsivil <unsivilaudio@gmail.com>
2022-06-18 11:54:35 -0600

    end of section 6

--- rbay/src/services/queries/items/items.ts
+++ rbay/src/services/queries/items/items.ts
@@ -1,7 +1,27 @@
 import type { CreateItemAttrs } from '$services/types';
+import { client } from '$services/redis';
+import { serialize } from './serialize';
+import { genId } from '$services/utils';
+import { itemsKey } from '$services/keys';
+import { deserialize } from './deserialize';
 
-export const getItem = async (id: string) => {};
+export const getItem = async (id: string) => {
+	const item = await client.hGetAll(itemsKey(id));
+
+	if (!Object.keys(item).length) {
+		return null;
+	}
+
+	return deserialize(id, item);
+};
 
 export const getItems = async (ids: string[]) => {};
 
-export const createItem = async (attrs: CreateItemAttrs, userId: string) => {};
+export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
+	const id = genId();
+	const serialized = serialize(attrs);
+
+	await client.hSet(itemsKey(id), serialized);
+
+	return id;
+};
--- rbay/src/services/queries/users.ts
+++ rbay/src/services/queries/users.ts
@@ -1,8 +1,38 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
+import { client } from '$services/redis';
+import { usersKey } from '$services/keys';
 
 export const getUserByUsername = async (username: string) => {};
 
-export const getUserById = async (id: string) => {};
+export const getUserById = async (id: string) => {
+	const user = await client.hGetAll(usersKey(id));
 
-export const createUser = async (attrs: CreateUserAttrs) => {};
+	if (!Object.keys(user).length) {
+		return null;
+	}
+
+	return deserialize(id, user);
+};
+
+export const createUser = async (attrs: CreateUserAttrs) => {
+	const id = genId();
+	await client.hSet(usersKey(id), serialize(attrs));
+
+	return id;
+};
+
+const serialize = (user: CreateUserAttrs) => {
+	return {
+		username: user.username,
+		password: user.password
+	};
+};
+
+const deserialize = (id: string, user: { [key: string]: string }) => {
+	return {
+		id,
+		username: user.username,
+		password: user.password
+	};
+};
