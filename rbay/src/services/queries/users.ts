53ee6935d9ac96b37f75b0223cd1b3a89904283e
unsivil <unsivilaudio@gmail.com>
2022-06-19 07:57:41 -0600

    end of section 9

--- rbay/src/services/queries/users.ts
+++ rbay/src/services/queries/users.ts
@@ -2,6 +2,7 @@ import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 import { client } from '$services/redis';
 import { usersKey } from '$services/keys';
+import { usernamesUniqueKey } from '$services/keys';
 
 export const getUserByUsername = async (username: string) => {};
 
@@ -17,7 +18,14 @@ export const getUserById = async (id: string) => {
 
 export const createUser = async (attrs: CreateUserAttrs) => {
 	const id = genId();
+
+	const exists = await client.sIsMember(usernamesUniqueKey(), attrs.username);
+	if (exists) {
+		throw new Error('Username is taken');
+	}
+
 	await client.hSet(usersKey(id), serialize(attrs));
+	await client.sAdd(usernamesUniqueKey(), attrs.username);
 
 	return id;
 };
--- rbay/tailwind.config.cjs
+++ rbay/tailwind.config.cjs
@@ -1,6 +1,11 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		container: {
+			padding: {
+				DEFAULT: '4rem'
+			}
+		},
 		extend: {}
 	},
 	plugins: [require('@tailwindcss/typography')]
