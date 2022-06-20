3fad38d9690fa45d3d03acc59ac15d553722d19b
unsivil <unsivilaudio@gmail.com>
2022-06-20 09:52:11 -0600

    end of section 16

--- rbay/src/services/queries/views.ts
+++ rbay/src/services/queries/views.ts
@@ -2,12 +2,13 @@ import { client } from '$services/redis';
 import { itemsKey, itemsByViewsKey, itemsViewsKey } from '$services/keys';
 
 export const incrementView = async (itemId: string, userId: string) => {
-	const inserted = await client.pfAdd(itemsViewsKey(itemId), userId);
+	return client.incrementView(itemId, userId);
+	// const inserted = await client.pfAdd(itemsViewsKey(itemId), userId);
 
-	if (inserted) {
-		return Promise.all([
-			client.hIncrBy(itemsKey(itemId), 'views', 1),
-			client.zIncrBy(itemsByViewsKey(), 1, itemId)
-		]);
-	}
+	// if (inserted) {
+	// 	return Promise.all([
+	// 		client.hIncrBy(itemsKey(itemId), 'views', 1),
+	// 		client.zIncrBy(itemsByViewsKey(), 1, itemId)
+	// 	]);
+	// }
 };
--- rbay/src/services/redis/client.ts
+++ rbay/src/services/redis/client.ts
@@ -1,11 +1,47 @@
-import { createClient } from 'redis';
+import { createClient, defineScript } from 'redis';
+import { itemsKey, itemsByViewsKey, itemsViewsKey } from '$services/keys';
 
 const client = createClient({
 	socket: {
 		host: process.env.REDIS_HOST,
 		port: parseInt(process.env.REDIS_PORT)
 	},
-	password: process.env.REDIS_PW
+	password: process.env.REDIS_PW,
+	scripts: {
+		addOneAndStore: defineScript({
+			NUMBER_OF_KEYS: 1,
+			SCRIPT: `
+                return redis.call('SET', KEYS[1], 1 + tonumber(ARGV[1]))
+            `,
+			transformArguments(key: string, value: number) {
+				return [key, value.toString()];
+			},
+			transformReply(reply: any) {
+				return reply;
+			}
+		}),
+		incrementView: defineScript({
+			NUMBER_OF_KEYS: 3,
+			SCRIPT: `
+                local itemsViewsKey = KEYS[1]
+                local itemsKey = KEYS[2]
+                local itemsByViewsKey = KEYS[3]
+                local itemId = ARGV[1]
+                local userId = ARGV[2]
+
+                local inserted = redis.call('PFADD', itemsViewsKey, userId)
+
+                if inserted == 1 then
+                    redis.call('HINCRBY', itemsKey, 'views', 1)
+                    redis.call('ZINCRBY', itemsByViewsKey, 1, itemId)
+                end
+            `,
+			transformArguments(itemId: string, userId: string) {
+				return [itemsViewsKey(itemId), itemsKey(itemId), itemsByViewsKey(), itemId, userId];
+			},
+			transformReply() {}
+		})
+	}
 });
 
 client.on('error', (err) => console.error(err));
